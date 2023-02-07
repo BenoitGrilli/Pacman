c4eb37adba404186cc3399852d375aaa9283bd24
benwa06 <benwa06@hotmail.fr>
2023-02-07 13:19:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,137 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+//pas besoin de mettre window.innerWidth , il comprend tout seul
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Boundary {
+    static width = 40
+    static height = 40
+    constructor({ position }) {
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+    
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    }
+}
+
+class Player {
+    constructor({ position,velocity }) {
+
+        this.position = position
+        this.velocity = velocity 
+        this.radius = 15
+    }
+        
+    
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'yellow'
+        c.fill()
+        c.closePath()
+    }
+// player mouvements
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+
+const boundaries = []
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width /2,
+        y: Boundary.height + Boundary.height /2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const map = [
+    ['-','-','-','-','-','-'], 
+    ['-',' ',' ',' ',' ','-'],
+    ['-',' ','-','-',' ','-'],
+    ['-',' ',' ',' ',' ','-'], 
+    ['-','-','-','-','-','-'],
+]
+// i stand for index
+map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        switch (symbol) {
+            case '-':
+                boundaries.push(
+                    new Boundary({
+                        position: {
+                            x: Boundary.width * j,
+                            y: Boundary.height * i
+                        }
+                    })
+                )    
+                break
+        }
+    })      
+})
+
+//animation mouvements
+function animate() {
+  requestAnimationFrame(animate)
+  // supprimer la trace jaune
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  boundaries.forEach((boundary) => {
+boundary.draw()
+})
+
+    player.update()
+}
+
+animate()
+
+
+addEventListener('keydown', ({key}) => {
+    switch (key) {
+        case'z':
+            player.velocity.y = -5
+            break
+        case'q':
+            player.velocity.x = -5
+            break
+        case'd':
+            player.velocity.x = 5
+            break
+        case's':
+            player.velocity.y = 5
+            break                        
+    }
+    console.log(player.velocity)
+})
+
+
+addEventListener('keyup', ({key}) => {
+    switch (key) {
+        case'z':
+            player.velocity.y = 0
+            break
+        case'q':
+            player.velocity.x = 0
+            break
+        case'd':
+            player.velocity.x = 0
+            break
+        case's':
+            player.velocity.y = 0
+            break                        
+    }
+    console.log(player.velocity)
+})
